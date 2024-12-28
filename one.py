078f0f8c31a483d4209ee182a09268bfc563ff93
Hangyaku <hangyakuzero@gmail.com>
2024-12-28 19:27:26 +0530

    some basic programs for fun lol

---
+++ one.py
@@ -0,0 +1,8 @@
+password = "bigbigbigcode"
+typedpass = input("enter your password")
+if typedpass == password:
+    print("access granted")
+elif typedpass == "1234":
+    print("only an idiot would have that as their password")
+else:
+    print("wrong password")
