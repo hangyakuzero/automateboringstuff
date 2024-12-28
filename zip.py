6f7ca2abc1e29d5d1943d81e08cce437743226c2
Hangyaku <hangyakuzero@gmail.com>
2024-12-28 20:30:01 +0530

    added zip method

---
+++ zip.py
@@ -0,0 +1,4 @@
+li = ["apple", "strawberry", "madmax", "batman", "king", "warrior"]
+di = {"john": 24, "jack": 30, "daniels": 45, "jhonnie": 50, "walker": 30}
+for l, name in zip(li, di):
+    print(f"list item:{l}, name:{name}")
