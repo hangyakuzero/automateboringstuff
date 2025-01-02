7f3c8ded7ea7da820b1e853fdfe51c6e3d55b4e2
Hangyaku <hangyakuzero@gmail.com>
2025-01-02 19:40:33 +0530

    fixed soy code

--- substring.py
+++ substring.py
@@ -1,17 +1,17 @@
 def solution(s):
-    l = []
-    lon = []
-    for i in s:
-        if i in l:
-            lon.append(len(l))
-            l.clear()
-            l.append(i)
-        else:
-            l.append(i)
-
-    return max(lon)
+    start = 0
+    charmap = {}
+    maxlen = 0
+    for i in range(len(s)):
+        if s[i] in charmap and charmap[s[i]] >= start:
+            start = charmap[s[i]] + 1
+        charmap[s[i]] = i
+        maxlen = max(maxlen, i - start + 1)
+    return maxlen
 
 
 print(solution("abcabcbb"))
 print(solution("bbbbbbbbbbb"))
 print(solution("abcbbbasdfgghdf"))
+print(solution("aab"))
+print(solution("dvdf"))
